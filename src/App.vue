ce1be23499167620fe9117028c89de3c3f9db00f
Aron Tolentino <arontolentino@outlook.com>
2019-08-17 16:53:44 -0400

    Set up initial Vue project

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+    YouTube Browser
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  name: "app",
+  components: {
+  }
+};
+</script>
+
+<style>
+
+</style>
