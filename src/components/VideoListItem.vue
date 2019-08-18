5f87b2399a397d4a5770d2387248bf911fc6b1f3
Aron Tolentino <arontolentino@outlook.com>
2019-08-18 11:45:38 -0400

    Implement video thumbnail by computer property

--- src/components/VideoListItem.vue
+++ src/components/VideoListItem.vue
@@ -1,10 +1,18 @@
 <template>
-  <li class="list-group-item">{{ video.snippet.title }}</li>
+  <li class="list-group-item">
+    <img :src="thumbnailUrl" />
+    {{ video.snippet.title }}
+  </li>
 </template>
 
 <script>
 export default {
   name: 'VideoListItem',
-  props: ['video']
+  props: ['video'],
+  computed: {
+    thumbnailUrl() {
+      return this.video.snippet.thumbnails.default.url;
+    }
+  }
 };
 </script>
