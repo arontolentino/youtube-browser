4781665dffa7b28f9e197f37b836f7980407e313
Aron Tolentino <arontolentino@outlook.com>
2019-08-17 23:27:17 -0400

    Pass video response data from App to VideoList component

--- src/App.vue
+++ src/App.vue
@@ -1,34 +1,46 @@
 <template>
   <div id="app">
     <SearchBar @termChange="onTermChange"></SearchBar>
+    <VideoList :videos="videos"></VideoList>
   </div>
 </template>
 
 <script>
 import axios from 'axios';
-import SearchBar from './components/SearchBar'
+import SearchBar from './components/SearchBar';
+import VideoList from './components/VideoList';
+
 const API_KEY = 'AIzaSyAofqsGyAx04MDgDNF-5zqo5lYooD-UPBY';
 
 export default {
   name: 'App',
+  data: function() {
+    return {
+      videos: []
+    };
+  },
   components: {
-    SearchBar
+    SearchBar,
+    VideoList
   },
   methods: {
     onTermChange(searchTerm) {
-      axios.get('https://www.googleapis.com/youtube/v3/search', {
-        params: {
-          key: API_KEY,
-          type: 'video',
-          part: 'snippet',
-          q: searchTerm
-        }
-      }).then(response => console.log(response));
+      axios
+        .get('https://www.googleapis.com/youtube/v3/search', {
+          params: {
+            key: API_KEY,
+            type: 'video',
+            part: 'snippet',
+            q: searchTerm
+          }
+        })
+        .then(response => {
+          this.videos = response.data.items;
+        });
     }
   }
 };
 </script>
 
 <style>
-
 </style>
